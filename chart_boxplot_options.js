c722e56eb0a5195b156ecbcf49598b1bf14734bd
dejong <ryan.dejong@avl.com>
2025-04-20 09:59:58 -0400

    refactor: update boxplot chart options and improve data handling for weather visualization

--- chart_boxplot_options.js
+++ chart_boxplot_options.js
@@ -22,7 +22,7 @@ const chart_boxplot_options = {
     ],
     chart: {
         type: 'boxPlot',
-        height: 250,
+        height: 350,
         background: '#000',
         toolbar: {
             show: false,
@@ -49,11 +49,22 @@ const chart_boxplot_options = {
     theme: {
         mode: 'dark',
     },
-    colors: ['#008FFB', '#FEB019', '#008FFB', '#FEB019', '#008FFB'],
-    _markers: {
+    // colors: ['#008FFB', '#FEB019', '#008FFB', '#FEB019', '#008FFB'],
+    dataLabels: {
+        enabled: true,
+        enabledOnSeries:[0, 1],
+        offsetY: -10,
+        style: {
+            fontSize: '12px',
+            colors: ['#008FFB', '#FEB019', '#008FFB', '#FEB019', '#008FFB'],
+        },
+    },
+    markers: {
         colors: ['#fff', '#fff', '#fff', '#fff', '#fff'],
         strokeColors: ['#008FFB', '#FEB019', '#008FFB', '#FEB019', '#008FFB'],
+        // strokeColors: ['#fff', '#fff', '#fff', '#fff', '#fff'],
         strokeWidth: 2,
+        size:[0, 2, 0, 0, ],
     },
     plotOptions: {
         boxPlot: {
@@ -70,7 +81,7 @@ const chart_boxplot_options = {
         },
     },
     title: {
-        text: 'BoxPlot chart with outliers',
+        text: 'Forecast Overview',
         align: 'left',
     },
     xaxis: {
